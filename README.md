b30b34853bdc915d4ae3f5a0b9bee56911e8fd68
Michal Korotkiewicz <michal.korotkiewicz@smartframe.io>
2022-10-13 10:57:07 +0200

    Add source code + update README.md

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # c2pa-thumbs
 Fetch all thumbs from file with c2pa
+
+## Installation
+
+cargo install --git https://github.com/SmartFrame-Technologies/c2pa-thumbs.git --tag 0.1.0
+
+## How to use it
+
+Run from command line
+```shell
+c2pa-thumbs {file-path}
+```
+
+## Result
+
+Output of command containt json encoded with 2 keys:
+```json
+{
+    "thumnail": "main_thumnail_base64_encoded_source",
+    "thumnails": [
+        "ingrendient_thumbnail_base64_encoded_source",
+        "ingrendient_1_thumbnail_base64_encoded_source",
+        ...
+    ]
+}
+```
---
+++ src/main.rs
@@ -0,0 +1,40 @@
+use c2pa::ManifestStore;
+use std::env;
+use std::error::Error;
+use std::process;
+use base64;
+use serde::{Serialize, Deserialize};
+use serde_json;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct ManifestThumbs {
+    thumbnail: Option<String>,
+    thumbnails: Vec<String>
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        println!("Please provide file");
+        process::exit(0x0100);
+    }
+
+    let file_path = &args[1];
+    let manifest_store = ManifestStore::from_file(file_path)?;
+    let manifest = manifest_store.get_active().unwrap();
+    let ingredients = manifest.ingredients();
+    let mut thumbs = ManifestThumbs { thumbnail: None, thumbnails: Vec::new()};
+    if let Some((format, image)) = manifest.thumbnail() {
+        thumbs.thumbnail = Some(format!("data:{};charset=utf-8;base64,{}", format, base64::encode(&image)));
+    }
+
+    for i in 0..ingredients.len() {
+        if let Some((format, image)) = ingredients[i].thumbnail() {
+            thumbs.thumbnails.push(format!("data:{};charset=utf-8;base64,{}", format, base64::encode(&image)));
+        }
+    }
+
+    println!("{}", serde_json::to_string(&thumbs).unwrap());
+    Ok(())
+}
